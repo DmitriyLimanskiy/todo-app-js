05eaff340f2e297bdb0f5e86ceb6dec07604f41c
Dmitriy Limanskiy <dmiitrii19s@gmail.com>
2025-10-21 14:20:45 +0300

    Поправил структуру проекта, поправил пути в index.html, изменил название переменных на более читаемые, улучшил обработчик пустой строки

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+const todoForm = document.getElementById('todo-form');
+const todoInput = document.getElementById('todo-input');
+const todoList = document.getElementById('todo-list');
+
+// обработка отправки формы
+todoForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // чтобы не перезагружать страницу
+
+    const taskText = todoInput.value.trim(); // убираем пробелы в начале и в конце текста
+
+    if (!taskText) {
+        // проверка на пустую строку/null/undefind ввода
+        return;
+    }
+
+    const li = document.createElement('li'); // создание нового элемента li
+    li.textContent = taskText; // вставляем текст в новый элемент li
+
+    todoList.appendChild(li);
+    todoInput.value = ''; // очистка поля ввода
+});

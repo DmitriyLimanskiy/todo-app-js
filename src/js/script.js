4d8afd8fbc8791a2f2368c694dff840a84cd550b
DmitriyLimanskiy <dmiitrii19s@gmail.com>
2025-10-23 14:12:43 +0300

    Revert "Спринт 2: выполнение, удаление, стили"

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,7 @@ const todoList = document.getElementById('todo-list');
 
 // обработка отправки формы
 todoForm.addEventListener('submit', (event) => {
-    event.preventDefault(); // убираем дефолтное действие, чтобы не перезагружать страницу
+    event.preventDefault(); // чтобы не перезагружать страницу
 
     const taskText = todoInput.value.trim(); // убираем пробелы в начале и в конце текста
 
@@ -14,49 +14,8 @@ todoForm.addEventListener('submit', (event) => {
     }
 
     const li = document.createElement('li'); // создание нового элемента li
-    li.classList.add('todo-item'); // добавляем к элементу li класс
-
-    const span = document.createElement('span');
-    span.classList.add('todo-text');
-    span.textContent = taskText;
-
-    const completeBtn = document.createElement('button');
-    completeBtn.classList.add('complete-btn');
-    completeBtn.textContent = '✅';
-
-    const deleteBtn = document.createElement('button');
-    deleteBtn.classList.add('delete-btn');
-    deleteBtn.textContent = '❌';
-
-    // добавляем к элементу li детей
-    li.appendChild(span);
-    li.appendChild(completeBtn);
-    li.appendChild(deleteBtn);
+    li.textContent = taskText; // вставляем текст в новый элемент li
 
     todoList.appendChild(li);
     todoInput.value = ''; // очистка поля ввода
-});
-
-// обработчик событий при нажатии на завершить и удалить задачу
-todoList.addEventListener('click', (event) => {
-    // найдем элемент с классом кнопка
-    const button = event.target.closest('button');
-    if (!button) return;
-
-    // найдем ближайший li с классом todo-item
-    const listItem = button.closest('.todo-item');
-    if (!listItem) return;
-
-    console.log('Кликнули по', event.target);
-    console.log('button:', button);
-    console.log('listItem:', listItem);
-
-    if (button.classList.contains('delete-btn')) {
-        listItem.remove(); // удалить задачу
-    }
-
-    if (button.classList.contains('complete-btn')) {
-        listItem.classList.toggle('completed');
-        // добавить класс выполнено, чтобы поменять стиль
-    }
 });
